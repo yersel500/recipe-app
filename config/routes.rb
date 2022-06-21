97a9f79f9f2597defce79e98b603b32c3a6a457d
yersel500 <yersel500@gmail.com>
2022-06-21 15:22:09 -0500

    add routes for recipe_foods

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  root to: "recipes#index"
+  root to: 'recipes#index'
 
   resources :users do
-    resources :recipes
     resources :foods
+    resources :recipes do
+      resources :recipe_foods
+    end
   end
 end
