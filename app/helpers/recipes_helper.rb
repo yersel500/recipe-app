276ceeb544b2943f8b36fa751b8619ada705f687
mengiefen <mengiefen@gmail.com>
2022-06-22 02:01:00 +0300

    include the calculate price method in recipe_helper

--- app/helpers/recipes_helper.rb
+++ app/helpers/recipes_helper.rb
@@ -2,4 +2,15 @@ module RecipesHelper
   def public?(recipe)
     recipe.public
   end
+
+  def calulate_price(recipe)
+    foods = recipe.foods
+    recipe_foods = recipe.recipe_foods
+    price = 0
+
+    foods.each do |food|
+      price += recipe_foods.find_by(food_id: food.id).quantity * food.price
+    end
+    price
+  end
 end
