0ab61d5c122216891c10edce666a76b4f73fd6bf
mengiefen <mengiefen@gmail.com>
2022-06-21 21:37:14 +0300

    add new and create action in recipesController

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -5,9 +5,26 @@ class RecipesController < ApplicationController
     @recipes = @user.recipes.all
   end
 
+  def new
+    @recipe = Recipe.new
+  end
+
   def show
     @recipe = Recipe.includes(:recipe_foods).find(params[:id])
+    @recipe_foods = @recipe.recipe_foods
     @foods = Food.all
+  end
+
+  def create
+    @user = User.find(params[:user_id])
+    @recipe = @user.recipes.new(recipe_params)
+    if @recipe.save
+      flash[:notice] = 'Recipe was successfully created'
+      respond_to? user_recipes_path(user_id: params[:user_id])
+    else
+      flash[:alert] = "Couldn't create the recipe!"
+      render :new
+    end
   end
 
   def destroy
@@ -19,5 +36,10 @@ class RecipesController < ApplicationController
       flash[:alert] = 'Unable to delete the recipe.'
       render user_recipe_path(@recipe)
     end
+  end
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description,
+                                   :public)
   end
 end
