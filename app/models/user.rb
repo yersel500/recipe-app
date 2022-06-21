97a5d241c281cbd89ad9675df492676c904f4fcd
mengiefen <mengiefen@gmail.com>
2022-06-21 23:29:58 +0300

    add a method to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,14 +2,21 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-    :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable
 
   has_many :recipes
   has_many :foods
-
   validates :name, presence: true
 
   def admin?
     role == 'admin'
+  end
+
+  def present?
+    id != nil
+  end
+
+  def public?
+    public
   end
 end
