a601f946885791710c816c677815d5ff0a208990
yersel500 <yersel500@gmail.com>
2022-06-22 11:08:02 -0500

    Add custom routes to delete foods

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  root to: "recipes#public_recipes"
+  root to: 'recipes#public_recipes'
 
   resources :users do
     resources :foods
@@ -11,5 +11,6 @@ Rails.application.routes.draw do
   end
 
   get '/users/:user_id/shopping', to: 'recipes#shopping_list', as: 'recipe_shopping'
-  get "/public_list",  to: "recipes#public_recipes", as: "public_recipes"
+  get '/public_list', to: 'recipes#public_recipes', as: 'public_recipes'
+  delete '/users/:user_id/foods/:id/delete', to: 'utilities#delete_food', as: 'delete_food'
 end
