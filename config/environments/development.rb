3e521e8bc8946434b2eb6e78370daa29ed7c2d6d
mengiefen <mengiefen@gmail.com>
2022-06-23 00:50:11 +0300

    configure bullet gem to detect N+1 queries

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -70,4 +70,12 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.perform_deliveries = true
+
+  config.after_initialize do
+    Bullet.enable = true
+    Bullet.alert = true
+    Bullet.bullet_logger = true
+    Bullet.console = true
+    Bullet.rails_logger = true
+  end
 end
