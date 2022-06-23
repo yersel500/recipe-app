4de4f61d1994fa2454a6aae8fe5bf1e99fc533fd
yersel500 <yersel500@gmail.com>
2022-06-23 00:54:25 -0500

    Add seed data to test

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.create(name: 'Lilly', email: 'lilly@gmail.com', password: 'lilly12345', password_confirmation: 'lilly12345',
+            confirmed_at: DateTime.now)
+
+Food.create(user: User.first, name: 'apple', measurement_unit: 'un', price: 1)
+Food.create(user: User.first, name: 'orange', measurement_unit: 'un', price: 2)
+Food.create(user: User.first, name: 'papaya', measurement_unit: 'un', price: 3)
+Food.create(user: User.first, name: 'banana', measurement_unit: 'un', price: 4)
